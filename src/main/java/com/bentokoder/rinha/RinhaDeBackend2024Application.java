c8902e519560e85d5f89b9d24fb404af14a3ad41
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2024-02-19 15:52:02 -0300

    start project

---
+++ src/main/java/com/bentokoder/rinha/RinhaDeBackend2024Application.java
@@ -0,0 +1,13 @@
+package com.bentokoder.rinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaDeBackend2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaDeBackend2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bentokoder/rinha/RinhaDeBackend2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bentokoder.rinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaDeBackend2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
