a09e2b487374dd8317913a8dbdfb045833c4b934
João Gabriel Bento <joaogabriel.bmm@gmail.com>
2024-02-22 20:36:13 -0300

    feat: criar os casos de testes do serviço de transacao

--- src/test/java/com/bentokoder/rinha/servicos/TransacaoServicoTest.java
+++ src/test/java/com/bentokoder/rinha/servicos/TransacaoServicoTest.java
@@ -4,13 +4,13 @@ import com.bentokoder.rinha.dtos.TransacaoRequestDTO;
 import com.bentokoder.rinha.entidade.Cliente;
 import com.bentokoder.rinha.entidade.Tipos;
 import com.bentokoder.rinha.repositorio.TransacaoRepositorio;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -26,14 +26,16 @@ class TransacaoServicoTest {
     @InjectMocks
     private TransacaoServico transacaoServico;
 
+
     @BeforeEach
     void setup(){
         MockitoAnnotations.initMocks(this);
     }
 
+
     @Test
     @DisplayName("deve realizar o debito com sucesso.")
-    void executarTransacaoTipoDebito(){
+    void executarTransacaoTipoDebitoCaso1(){
         Cliente cliente = new Cliente(1, 100000, 0);
         TransacaoRequestDTO transacao = new TransacaoRequestDTO(1000, Tipos.D, "something");
         when(clienteServico.obterClientePeloId(1)).thenReturn(cliente);
@@ -46,5 +48,55 @@ class TransacaoServicoTest {
 
         assertEquals(-1000, clienteAposTransacao.getSaldo());
     }
+
+    @Test
+    @DisplayName("deve falhar a tentativa de debito devido a falta de limite.")
+    void executarTransacaoTipoDebitoCaso2(){
+        Cliente cliente = new Cliente(1, 1000, 0);
+        TransacaoRequestDTO transacao = new TransacaoRequestDTO(1001, Tipos.D, "something");
+        when(clienteServico.obterClientePeloId(1)).thenReturn(cliente);
+
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            Cliente clienteAposTransacao = transacaoServico.executarTransacao(transacao, cliente.getId());
+        });
+
+        verify(clienteServico, times(1)).obterClientePeloId(1);
+        verify(clienteServico, times(0)).salvarCliente(any());
+        verify(transacaoRepositorio, times(0)).save(any());
+
+    }
+
+    @Test
+    @DisplayName("deve falhar ao tentar realizar um transacao para um cliente não cadastrado.")
+    void executarTransacaoTipoDebitoCaso3(){
+        TransacaoRequestDTO transacao = new TransacaoRequestDTO(1001, Tipos.D, "something");
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            Cliente clienteAposTransacao = transacaoServico.executarTransacao(transacao, 1);
+        });
+
+        verify(clienteServico, times(1)).obterClientePeloId(1);
+        verify(clienteServico, times(0)).salvarCliente(any());
+        verify(transacaoRepositorio, times(0)).save(any());
+
+    }
+
+    @Test
+    @DisplayName("deve realizar um credito com sucesso.")
+    void executarTransacaoTipoCredito(){
+        Cliente cliente = new Cliente(1, 100000, 0);
+        TransacaoRequestDTO transacao = new TransacaoRequestDTO(1000, Tipos.C, "something");
+        when(clienteServico.obterClientePeloId(1)).thenReturn(cliente);
+
+        Cliente clienteAposTransacao = transacaoServico.executarTransacao(transacao, cliente.getId());
+
+        verify(clienteServico, times(1)).obterClientePeloId(1);
+        verify(clienteServico, times(1)).salvarCliente(any());
+        verify(transacaoRepositorio, times(1)).save(any());
+
+        assertEquals(1000, clienteAposTransacao.getSaldo());
+        assertEquals(100000, clienteAposTransacao.getLimite());
+    }
+
 
 }
